a20fae081dd6916ef793e5ff5dd55a2ee5991ce3
Fano Raiza <fano@Fanos-MacBook-Pro.local>
2025-01-14 21:15:02 +0300

    some changes

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-#!/bin/sh
 set -e
 
 echo "Starting Flask application..."
-gunicorn --bind 0.0.0.0:5000 --workers 4 --threads 2 "app:app"
+gunicorn --bind 0.0.0.0:5000 --workers 4 --threads 2 --access-logfile - --error-logfile - "app:app"
